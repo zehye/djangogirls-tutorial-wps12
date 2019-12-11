e488969ab90caeac726c46d6f9b707486a5a876d
jihye <zehye.01@gmail.com>
2019-12-11 14:41:57 +0900

    views/post_list에서 복잡한 html 작성

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post List</h1></body></html>')
+    return HttpResponse(
+        '<html>'
+        '<body'
+        '<h1>Post List</h1>'
+        '</body>'
+        '</html>'
+    )
