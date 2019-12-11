bd0b072eed8ca8dcf6cb3d22ebb0008e97a146c3
jihye <zehye.01@gmail.com>
2019-12-11 15:40:34 +0900

    template을 찾을 경로를 serring.TEMPLATE에 정하고, post_list view에서는 render()함수를 사용해 템플릿 파일을 text로 변환하고 응답하도록 함

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 
 def post_list(request):
-    return HttpResponse(
-        '<html>'
-        '<body'
-        '<h1>Post List</h1>'
-        '</body>'
-        '</html>'
-    )
+    # print('h:', HttpResponse(post_list))
+    # Templates 을 찾을 경로에서 post_list.html 찾아 그 파일을 text 로 만들어서 HttpResponse 형태로 동작한다
+    # 위 기능을 하는 shortcut 함수
+
+    # content = loader.render_to_string('post_list.html', None, request)
+    # return HttpResponse(content)
+    return render(request, 'post_list.html')
+
--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print('b:', BASE_DIR)
+print('t:', TEMPLATES_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,10 +57,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
