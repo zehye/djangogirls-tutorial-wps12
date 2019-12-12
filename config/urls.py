9d03b1738e713e51541544fbd8dc62b1147913cf
jihye <zehye.01@gmail.com>
2019-12-12 15:53:38 +0900

    urls.py에 name설정 후 이에 알맞게 templates 수정

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from blog.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('post-list/', post_list),
-    path('post-detail/<int:pk>/', post_detail)
+    path('post-list/', post_list, name='post-list-name'),
+    path('post-detail/<int:pk>/', post_detail, name='post-detail-name')
 ]
