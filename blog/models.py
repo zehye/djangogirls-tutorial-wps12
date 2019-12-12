55157f0f5bb3728b4a4c3d8a3aabf0284e37bcf4
jihye <zehye.01@gmail.com>
2019-12-12 13:05:31 +0900

    blog 모델에 맞는 post_list.html 작성

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,22 @@
-from django.db import models
-
-# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField(blank=True)
     created_date = models.DateTimeField(
-            default=timezone.now)
+        default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
-    def __str__(self):
-        return self.title
